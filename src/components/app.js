b8ce00f3720d393f16f9d7dbf6a5aae1a219e0eb
Craig_Donavin <crdonavin@gmail.com>
2020-08-11 21:11:00 -0500

    value extract const pseudo code

--- src/components/app.js
+++ src/components/app.js
@@ -2,4 +2,18 @@ class App {
     constructor(){
         this.initBindingsAndEventListeners()
     }
+
+    /**const  valueExtractor = (input) => {
+  let stringBlob = input.split("u'").join("'").replace(/'/g, '"')
+  let arr1 = stringBlob.split('},')
+  let arr2 = arr1.map(e => e.endsWith(']') ? e+"}" : e)
+  let arrParse = arr2.map(JSON.parse)
+  let values = []
+  for (let el in arrParse){
+  values.push(arrParse[el]['__pk'][1])}
+  arr2=[]
+  arr1=[]
+  return values
+}
+console.log(valueExtractor(input)) */
 }
