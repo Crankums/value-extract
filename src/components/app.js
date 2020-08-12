666de0d8900fbdd2953d69f192a3d6f1a8b4f6f8
Craig_Donavin <crdonavin@gmail.com>
2020-08-12 15:04:04 -0500

    add div#results-box, valueExtractor() function

--- src/components/app.js
+++ src/components/app.js
@@ -4,19 +4,29 @@ class App {
     }
 
     initBindingsAndEventListeners(){
-        this.inputBox = document.querySelector("#input-box")
+        this.mainContainer = document.querySelector("#main-container")
+        this.sbmtBtn = this.mainContainer.querySelector("button")
+        this.sbmtBtn.addEventListener('click', this.handleSubmit.bind(this))
     }
-    /**const  valueExtractor = (input) => {
-  let stringBlob = input.split("u'").join("'").replace(/'/g, '"')
-  let arr1 = stringBlob.split('},')
-  let arr2 = arr1.map(e => e.endsWith(']') ? e+"}" : e)
-  let arrParse = arr2.map(JSON.parse)
-  let values = []
-  for (let el in arrParse){
-  values.push(arrParse[el]['__pk'][1])}
-  arr2=[]
-  arr1=[]
-  return values
-}
-console.log(valueExtractor(input)) */
-}
+
+    handleSubmit(e){
+        const input =  this.mainContainer.querySelector("#input-box")
+        const inputArg = input.value
+        const resultsBox = document.createElement("div")
+        resultsBox.setAttribute("id", "results-box")
+        this.mainContainer.appendChild(resultsBox)
+        console.log(this.valueExtractor(inputArg))
+    }
+    valueExtractor = (input) => {
+        let stringBlob = input.split("u'").join("'").replace(/'/g, '"')
+        let arr1 = stringBlob.split('},')
+        let arr2 = arr1.map(e => e.endsWith(']') ? e+"}" : e)
+        let arrParse = arr2.map(JSON.parse)
+        let values = []
+        for (let el in arrParse){
+        values.push(arrParse[el]['__pk'][1])}
+        arr2=[]
+        arr1=[]
+        return values
+        }
+    }
