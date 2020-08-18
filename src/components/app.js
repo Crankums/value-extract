dc98c0c2f83a99639d499a4f61b849cf91488de2
Craig_Donavin <crdonavin@gmail.com>
2020-08-17 22:01:36 -0500

    refactor valueExtractor(), clean up handleSubmit()

--- src/components/app.js
+++ src/components/app.js
@@ -11,47 +11,49 @@ class App {
 
     handleSubmit(e){
         const input =  this.mainContainer.children[0].value
-        // const inputArg = input.value
         const resultsBox = document.createElement("div")
         resultsBox.setAttribute("id", "results-box")
-        this.mainContainer.appendChild(resultsBox)
-        debugger
-        this.valueExtractor2(input)
-        // let valueTable = document.createElement("table")
+        this.mainContainer.appendChild(resultsBox)        
+        let valueTable = document.createElement("table")
         
-        // valueTable.setAttribute("id", "value-table")
-        // let row = valueTable.insertRow()
-        // let perRow = 1
-        // let count = 0
-        // let resultsArr= this.valueExtractor(inputArg)
-        // for (let i = 0; i<resultsArr.length; i++){
-        //     let cell = row.insertCell()
-        //     cell.innerHTML= resultsArr[i]
-        //     count ++
-        //     if (count%perRow == 0) {
-        //         row = valueTable.insertRow()
-        //     }
-        // }
-        // this.mainContainer.appendChild(valueTable)
+        valueTable.setAttribute("id", "value-table")
+        let row = valueTable.insertRow()
+        let perRow = 1
+        let count = 0
+        let resultsArr= this.valueExtractor(input)
+        for (let i = 0; i<resultsArr.length; i++){
+            let cell = row.insertCell()
+            cell.innerHTML= resultsArr[i]
+            count ++
+            if (count%perRow == 0) {
+                row = valueTable.insertRow()
+            }
+        }
+        this.mainContainer.appendChild(valueTable)
         // resultsBox.innerText = this.valueExtractor(inputArg)
 
     }
+    // valueExtractor = (input) => {
+    //     let stringBlob = input.split("u'").join("'").replace(/'/g, '"')
+    //     let arr1 = stringBlob.split('},')
+    //     let arr2 = arr1.map(e => e.endsWith(']') ? e+"}" : e)
+    //     let arrParse = arr2.map(JSON.parse)
+    //     let values = []
+    //     for (let el in arrParse){
+    //     values.push(arrParse[el]['__pk'][1])}
+    //     arr2=[]
+    //     arr1=[]
+    //     return values
+    //     }
+
     valueExtractor = (input) => {
         let stringBlob = input.split("u'").join("'").replace(/'/g, '"')
-        let arr1 = stringBlob.split('},')
-        let arr2 = arr1.map(e => e.endsWith(']') ? e+"}" : e)
-        let arrParse = arr2.map(JSON.parse)
-        let values = []
-        for (let el in arrParse){
-        values.push(arrParse[el]['__pk'][1])}
-        arr2=[]
-        arr1=[]
-        return values
+        stringBlob = stringBlob.split('},').map(e => e.endsWith(']') ? e+"}" : e).map(JSON.parse)
+        let returnArr = []
+        for (let el in stringBlob){
+                returnArr.push(stringBlob[el]['__pk'][1])
+            }
+            return returnArr
         }
 
-    valueExtractor2 = (input) => {
-        let stringBlob = input.split("u'").join("'").replace(/'/g, '"')
-        stringBlob = stringBlob.split('},')
-        console.log(stringBlob)
-    }
     }
